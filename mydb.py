918138d4b6d574268f44f1ca7e2a7dafba75c46e
jylin <jylin1@163.com>
2022-10-29 18:53:36 +0800

    <new>sql_DROP_TABLE_tableName

--- mydb.py
+++ mydb.py
@@ -128,7 +128,15 @@ def sql_CREATE_TABLE_use_dbName_create_tableName(db_name, table_name, table_deta
         str_table_detail = table_detail
     elif isinstance(table_detail, list):
         str_table_detail = one_str_from_strList(table_detail)
-    sql = "use " + str(db_name) + "; " + "CREATE TABLE " + str(table_name) + "(" + str_table_detail + ");"
 
     return sql 
+
+
+
+# Get SQL code: DROP TABLE tableName
+# T*
+def sql_DROP_TABLE_tableName(db_name, table_name):
+    sql = "use " + str(db_name) + "; " + "DROP TABLE " + str(table_name) + ";"
+    return sql 
+
 
