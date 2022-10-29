741115a203ee6221ff9507da6fb8ef132290a44e
jylin <jylin1@163.com>
2022-10-29 15:34:34 +0800

    <new>split_str_by_spList_ret_list

--- mydb.py
+++ mydb.py
@@ -19,9 +19,40 @@ def connect_db(user, password, host, port, database):
 
 
 # _________________________________________________________________________________________________________________
+# Split a str in list by split list, return a str splited by enter char
+def split_str_by_spList_ret_enterStr(str, split_list):
+    new_str = ''
+    for char_i in str:
+        if char_i != '\n' and char_i in split_list:
+            char_i = '\n'
+        new_str += char_i
+    return new_str
+
+   
+
+
+# _________________________________________________________________________________________________________________
+# Split a str in list by split list, return a str splited list
+def split_str_by_spList_ret_list(str, split_list):
+    new_str = ''
+    for char_i in str:
+        if char_i != '\n' and char_i in split_list:
+            char_i = '\n'
+        new_str += char_i
+    return new_str.split('\n')
+
+
+
+
+# _________________________________________________________________________________________________________________
 # Execute a SQL code
+# Add: execute multiple line code function
 # T*
 def SQL(cur, sql):
+    not_enter_strList = []
+    if '\n' in sql:
+        for str_i in sql.pli
+
     cur.execute(sql)
 
 
@@ -71,13 +102,12 @@ def get_version(cur):
 # _________________________________________________________________________________________________________________
 # Get SQL code: DROP_TABLE_IF_EXISTS_MENU
 # T*
-def sql_DROP_TABLE_IF_EXISTS_TableName(table_name):
+def sql_DROP_TABLE_IF_EXISTS_tableName(table_name):
     return "DROP TABLE IF EXISTS " + str(table_name) + ";"
 
 
 # _________________________________________________________________________________________________________________
 # Get SQL code: CREATE TABLE table_name(table_detail_list)
 # T*
-def sql_CREATE_TABLE_TableName(table_name, table_detail_list):
-
+def sql_CREATE_TABLE_tableName(table_name, table_detail_list):
     return "DROP TABLE IF EXISTS " + str(table_name) + ";"
