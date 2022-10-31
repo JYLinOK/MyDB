d1b762b07d6b345a7e51a583fe411e6299d52f95
jylin <jylin1@163.com>
2022-10-31 21:56:59 +0800

    <new>sql_SELECT_ALL_FROM_tabl_WHERE_column_NOT_LIKE_x

--- mydb.py
+++ mydb.py
@@ -520,3 +520,12 @@ def sql_SELECT_ALL_FROM_tabl_WHERE_column_LIKE_between_x(db_name, table_name, co
     print(sql)
     return sql 
 
+
+
+# _________________________________________________________________________________________________________________
+# Get SQL code:  SELECT * FROM table WHERE column NOT LIKE x
+# T*
+def sql_SELECT_ALL_FROM_tabl_WHERE_column_NOT_LIKE_x(db_name, table_name, column, x):
+    sql = "use " + str(db_name) + "; " + "SELECT * FROM " + str(table_name) + " WHERE " + str(column) + " NOT LIKE " + sql_str(x) + ";"
+    print(sql)
+    return sql 
